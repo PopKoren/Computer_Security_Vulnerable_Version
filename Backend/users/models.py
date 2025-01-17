6266ba422dca1193dd5fdfd3a4eedf5fa0e5505f
Koren Abugov <korenabugov@gmail.com>
2025-01-17 23:02:35 +0200

    1

--- Backend/users/models.py
+++
@@ -1,7 +0,0 @@
-# users/models.py
-from django.db import models
-
-class LoginAttempt(models.Model):
-    ip_address = models.GenericIPAddressField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    was_successful = models.BooleanField(default=False)
